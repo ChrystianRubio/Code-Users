033d3f4b3e008d0edb7851b3fa9cd9619cb4602c
ChrystianRubio <chrys.melo@outlook.com>
2023-10-25 01:08:03 -0400

    added pretty design for login

--- clientes/views.py
+++ clientes/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from .models import Cliente
 
 # Create your views here.
 def login(request):
+    
+    if request.method == "POST":
+
+        name = request.POST.get('name')
+        passwd = request.POST.get('pass')
+
+        if name != "" and passwd != "":
+            db = Cliente(name=name, password=passwd)
+            
+            db.save()
+    
+    elif request.method == "GET":
+        db = Cliente.objects.all()
+        
+        print(db)
+        
+ 
     return render(request, 'login.html')
+
+    
+
 
