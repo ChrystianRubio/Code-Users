3223bc027d676539674bca3b1aa99afb48d4ec3c
ChrystianRubio <chrys.melo@outlook.com>
2023-10-26 17:53:22 -0400

    inside file with remove in db

--- clientes/urls.py
+++ clientes/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 
     path('', views.login, name="login"),
     path('register/', views.create_user, name="create_user"),
+    path('inside/', views.del_user, name="del_user"),
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -4,22 +4,24 @@ from .models import Cliente
 # Create your views here.
 def login(request):
     
+    
     if request.method == "POST":
 
         name = request.POST.get('name')
         passwd = request.POST.get('pass')
+        
         acess_db = Cliente.objects.filter(name=name, password=passwd)
 
 
         if name != "" and passwd != "":
             if acess_db:
-                return render(request, 'inside.html', {})
+                return render(request, 'inside.html', {"name": name})
             else:
                 return render(request, 'login.html', {"error": "User not found"})
         else:
             return render(request, 'login.html', {"error": ""})
-   
-    
+
+
     elif request.method == "GET":
     
         return render(request, 'login.html', {})
@@ -50,4 +52,26 @@ def create_user(request):
 
     elif request.method == "GET":
         return render(request, 'register.html', {})
+
+
+def del_user(request):
+
+    print(request)
+
+    if request.method == "POST":
+
+        name = request.POST.get('name')
+        passwd = request.POST.get('pass')
+
+        acess_db = Cliente.objects.get(name=name)
+        acess_db.delete()
+        
+
+        return render(request, 'inside.html', {"msg_status": "Remove successful"})
+
+    elif request.method == "GET":
+        name = request.POST.get('name')
+        passwd = request.POST.get('pass')
+        print(name, passwd)
+        return render(request, 'inside.html', {"msg_status": ""})
 
